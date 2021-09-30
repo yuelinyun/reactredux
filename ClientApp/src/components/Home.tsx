1e86addf1dff7a56c1760dd38240589de2b4b49c
yuelinyun <yuelinyun@protonmail.com>
2021-09-30 03:34:34 +0000

    migrate db

--- ClientApp/src/components/Home.tsx
+++ ClientApp/src/components/Home.tsx
@@ -1,6 +1,39 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
+const testget = () => {
+    console.log('testget');
+    fetch("https://localhost:5001/api/todoitems")
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((res) => {
+      console.log(res);
+    });
+}
+
+const testpost = () => {
+  console.log('testpost');
+  fetch("https://localhost:5001/api/todoitems", {
+    method: 'POST',
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify({ Name: "yuelin", IsComplete: false}) // body data type must match "Content-Type" header
+  })
+  .then((res) => {
+    console.log(res);
+  });
+  
+}
+
 const Home = () => (
   <div>
     <h1>Hello, world!</h1>
@@ -17,6 +50,9 @@ const Home = () => (
       <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
     </ul>
     <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+  
+    <button onClick={testget}>Test GET</button>
+    <button onClick={testpost}>Test POST</button>
   </div>
 );
 
--- Models/TodoContext.cs
+++ Models/TodoContext.cs
@@ -3,6 +3,6 @@ using Microsoft.EntityFrameworkCore;
 namespace reactredux.Models {
     public class TodoContext: DbContext {
         public TodoContext(DbContextOptions<TodoContext> options) : base(options) {}
-        public DbSet<TodoContext> TodoItems { get; set;}
+        public DbSet<TodoItem> TodoItems { get; set;}
     }
 }
